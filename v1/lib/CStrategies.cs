4089f293da3e7df777ec953bbfcc15038c6fbc93
Sandeep L Chiluveru <sandeepnet@aol.com>
2026-07-01 02:02:19 +0530

    Add a new strategy strategy_3

--- v1/lib/CStrategies.cs
+++ v1/lib/CStrategies.cs
@@ -8,6 +8,11 @@ interface IStrategies
 {
     public void sayHello();
     public CSignal getSignal();
+    public SIG Strategy_1(in T_SIG tSig);
+    public SIG Strategy_2(in T_SIG tSig, in double totalTradeProfits);
+    public SIG Strategy_3(in T_SIG tSig, in double totalTradeProfits);
+    public SIG Strategy_4(in T_SIG tSig, in double totalTradeProfits);
+
 }
 public class CStrategies : IStrategies
 {
@@ -41,12 +46,12 @@ public class CStrategies : IStrategies
            && ((tSig.fsig30 == SIG.BUY) || (tSig.fsig30 == SIG.SELL))
         )
         {
-            Log.print($"Strategy_1: fsig5={tSig.fsig5}, fsig30={tSig.fsig30}, microWaveSIG={tSig.microWaveSIG}, baseSlopeSIG={tSig.baseSlopeSIG}");   
+            Log.print($"Strategy_1: fsig5={tSig.fsig5}, fsig30={tSig.fsig30}, microWaveSIG={tSig.microWaveSIG}, baseSlopeSIG={tSig.baseSlopeSIG}");
             return tSig.fsig30;
         }
         else
         {
-            Log.print($"Strategy_1: CLOSE");   
+            Log.print($"Strategy_1: CLOSE");
             return SIG.CLOSE;
         }
 
@@ -72,7 +77,7 @@ public class CStrategies : IStrategies
         && (totalTradeProfits >= 4.0)
         )
         {
-            Log.print($"Strategy_2: Profit CLOSE");   
+            Log.print($"Strategy_2: Profit CLOSE");
             return SIG.CLOSE;
         }
         else if (
@@ -82,7 +87,7 @@ public class CStrategies : IStrategies
            && (tSig.fsig30 == tSig.baseSlopeSIG)
         )
         {
-            Log.print($"Point 2: Strategy_2: fsig5={tSig.fsig5}, fsig30={tSig.fsig30}, fastSIG={tSig.fastSIG}, baseSlopeSIG={tSig.baseSlopeSIG}");   
+            Log.print($"Point 2: Strategy_2: fsig5={tSig.fsig5}, fsig30={tSig.fsig30}, fastSIG={tSig.fastSIG}, baseSlopeSIG={tSig.baseSlopeSIG}");
             return tSig.fsig30;
         }
         ////################################################################################################################
@@ -119,6 +124,37 @@ public class CStrategies : IStrategies
         {
             Log.print($"Point 3: Strategy_3: fsig5={tSig.fsig5}, fsig30={tSig.fsig30}, fastSIG={tSig.fastSIG}, baseSlopeSIG={tSig.baseSlopeSIG}");
             return tSig.fsig30;
+        }
+        //################################################################################################################
+        return SIG.NOSIG;
+    }
+
+    public SIG Strategy_4(in T_SIG tSig, in double totalTradeProfits)
+    {
+        //################################################################################################################
+        //############################# baseSlope Strategy [close on baseSlope = Close]
+        //################################################################################################################
+        if (
+          (tSig.slope30SIG == SIG.CLOSE)
+          && (tSig.fastSIG == SIG.HOLD)
+        )
+        {
+            Log.print($"Strategy_4: Signal CLOSE");
+            return SIG.CLOSE;
+        }
+        else if ((tSig.slope30SIG == SIG.CLOSE)
+        && (totalTradeProfits >= 4.0)
+        )
+        {
+            Log.print($"Strategy_4: Profit CLOSE");
+            return SIG.CLOSE;
+        }
+        else if (
+          (tSig.slope30SIG == SIG.BUY || tSig.slope30SIG == SIG.SELL)
+        )
+        {
+            Log.print($"Point 3: Strategy_4: slope30SIG={tSig.slope30SIG}");
+            return tSig.slope30SIG;
         }
         //################################################################################################################
         return SIG.NOSIG;
